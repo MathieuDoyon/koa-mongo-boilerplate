66ffcabf14a2125ea4c64b338c89f5bdf10fbb41
Mathieu Doyon <mathieu.doyon@ssense.com>
2017-05-11 23:04:21 -0400

    Test

--- .circleci/setup-gcloud.sh
+++ .circleci/setup-gcloud.sh
@@ -7,3 +7,8 @@ apt-get install -y python2.7
 wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-153.0.0-linux-x86_64.tar.gz
 tar -xzf google-cloud-sdk-153.0.0-linux-x86_64.tar.gz
 ./google-cloud-sdk/install.sh --usage-reporting false --command-completion true --additional-components alpha beta --quiet
+# sudo /opt/google-cloud-sdk/bin/gcloud --quiet components update kubectl
+
+which gcloud
+
+sudo gcloud --quiet components update kubectl
