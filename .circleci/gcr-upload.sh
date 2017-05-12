41eebe5dfb9c17b47b5651c7fa4663438633c06e
Mathieu Doyon <mathieu.doyon@ssense.com>
2017-05-12 13:08:34 -0400

    Test

--- .circleci/gcr-upload.sh
+++ .circleci/gcr-upload.sh
@@ -3,4 +3,4 @@ export PATH=$PATH:/home/circleci/google-cloud-sdk/bin
 docker build --build-arg NPM_TOKEN=${NPM_TOKEN} -t ${DOCKER_IMAGE_NAME}:$(git rev-parse --short HEAD) .
 
 # Build and push docker image
-gcloud docker push ${DOCKER_IMAGE_NAME}:$(git rev-parse --short HEAD)
+gcloud docker -- push ${DOCKER_IMAGE_NAME}:$(git rev-parse --short HEAD)
