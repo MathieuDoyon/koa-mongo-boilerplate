8507f34d2bfc839892c89a7917eeb862277117c6
Mathieu Doyon <mathieu.doyon@ssense.com>
2017-05-13 10:06:33 -0400

    Starting K8S deploy

---
+++ .circleci/k8s-deploy-staging.sh
@@ -0,0 +1,10 @@
+# echo "Performing rolling update for deployment $deployment using image ${DOCKER_IMAGE_NAME}"
+#   (kubectl rollout status deployment/${deployment} --namespace ${K8S_NAMESPACE} && \
+#     kubectl set image deployment/${deployment} --namespace ${K8S_NAMESPACE} ${container}=${DOCKER_IMAGE_NAME}) || \
+#     echo "Warning: Ignoring rolling update"
+
+K8S_TAG=$(git rev-parse --short HEAD)
+
+echo "K8S_TAG ${K8S_TAG}"
+
+# kubectl patch deployment $K8S_CONTAINER -p '{"spec":{"template":{"spec":{"containers":[{"name":"$K8S_CONTAINER","image":"$DOCKER_IMAGE_NAME:'"$K8S_TAG"'"}]}}}}'
--- .circleci/k8s-deploy.sh
+++
@@ -1,4 +0,0 @@
-echo "Performing rolling update for deployment $deployment using image ${docker_tagged_image}"
-  (kubectl rollout status deployment/${deployment} --namespace ${namespace} && \
-    kubectl set image deployment/${deployment} --namespace ${namespace} ${container}=${docker_tagged_image}) || \
-    echo "Warning: Ignoring rolling update"
