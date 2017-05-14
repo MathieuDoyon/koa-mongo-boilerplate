b84e92703419f9e3baf888e298beb855b343ff1c
Mathieu Doyon <mathieu.doyon@ssense.com>
2017-05-13 20:56:08 -0400

    Export PATH into deploy

--- .circleci/k8s-deploy-staging.sh
+++ .circleci/k8s-deploy-staging.sh
@@ -4,9 +4,15 @@
 #     echo "Warning: Ignoring rolling update"
 export PATH=$PATH:/home/circleci/google-cloud-sdk/bin
 
+GCLOUD_PROJECT_NAME=garage-gigs
+GCLOUD_CLUSTER_NAME=gigs-1
+GCLOUD_COMPUTE_ZONE=us-east1-d
+K8S_CONTAINER=circleci-node-mongo
+K8S_NAMESPACE=staging
+DOCKER_IMAGE_NAME=gcr.io/garage-gigs/circleci-node-mongo
 K8S_TAG=$(git rev-parse --short HEAD)
 
-echo "K8S_TAG ${K8S_TAG}"
+# echo "K8S_TAG ${K8S_TAG}"
 
 # kubectl patch deployment $K8S_CONTAINER -p '{"spec":{"template":{"spec":{"containers":[{"name":"$K8S_CONTAINER","image":"$DOCKER_IMAGE_NAME:'"$K8S_TAG"'"}]}}}}'
 kubectl patch rc $K8S_CONTAINER -n staging -p '{"spec":{"template":{"spec":{"containers":[{"name":"$K8S_CONTAINER","image":"$DOCKER_IMAGE_NAME:'"$K8S_TAG"'"}]}}}}'
