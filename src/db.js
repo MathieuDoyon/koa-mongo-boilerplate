0db96841bc76934a5be51f34309b9da9a284068c
Mathieu Doyon <mathieu.doyon@ssense.com>
2017-05-13 20:25:38 -0400

    Re-add mongo

--- src/db.js
+++ src/db.js
@@ -1,5 +1,5 @@
 import monk from 'monk'
 import config from './config'
 
-console.log('Connect DB to ', config.database.url);
+console.log('--Connect DB to ', config.database.url);
 export default monk(config.database.url)
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,31 +1,26 @@
 import Router from 'koa-router'
-// import db from '../db'
+import db from '../db'
 
 const router = new Router()
-// const Demo = db.get('demo')
+const Demo = db.get('demo')
 
 router.get('/', async ctx => {
-  // const data = await Demo.find({})
+  const data = await Demo.find({})
 
-  // ctx.body = data
-
-  ctx.body = [
-    {foo: 'bar'}
-  ]
+  ctx.body = data
 })
 
 router.post('/', async ctx => {
-  // const input = ctx.request.body
+  const input = ctx.request.body
 
-  // const inserted = await Demo.insert(input)
+  const inserted = await Demo.insert(input)
 
-  // if (inserted === null) {
-  //   ctx.status = 405
-  //   throw new Error('Error while creating object')
-  // }
+  if (inserted === null) {
+    ctx.status = 405
+    throw new Error('Error while creating object')
+  }
 
-  // ctx.body = inserted
-  ctx.body = ctx.request.body
+  ctx.body = inserted
 })
 
 router.get('/health', async ctx => {
